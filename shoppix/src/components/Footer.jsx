ddd821e63c475d54245e9620e7d77f750db18563
Murat <123808058+Muratkyn@users.noreply.github.com>
2023-10-26 10:42:14 +0200

    Update Footer.jsx

--- shoppix/src/components/Footer.jsx
+++ shoppix/src/components/Footer.jsx
@@ -13,8 +13,7 @@ const Footer = () => {
               <a>Contact us</a>
               <p>Design by MK</p>  
             </div>
-        </div>
-            
+        </div>    
     </div>
   )
 }
